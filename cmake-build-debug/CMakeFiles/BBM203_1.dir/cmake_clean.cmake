256a25df1e4a9f290d7cc710a629a2da385c1c9e
ymg <ymgozutok@gmail.cn>
2019-10-26 22:03:10 +0300

    ilk commit

---
+++ cmake-build-debug/CMakeFiles/BBM203_1.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "BBM203_1"
+  "BBM203_1.pdb"
+  "CMakeFiles/BBM203_1.dir/main.c.o"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/BBM203_1.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ cmake-build-debug/CMakeFiles/BBM203_1.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for BBM203_1.
+# This may be replaced when dependencies are built.
