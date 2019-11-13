c0d44b83a5780a322a57e87f1b04268210fe27a9
ymgoz <ymgozutok@gmail.com>
2019-11-13 16:11:00 +0300

    matstack

--- main.c
+++ main.c
@@ -10,7 +10,8 @@ typedef struct{
 
 typedef struct{
     char *name;
-    int length;
+    int x;
+    int y;
     int **arr;
 } matrix;
 //      LINE COUNT
@@ -34,50 +35,59 @@ int lineCount(const char *filename)
 
     return count;
 }
+
 //  ======== INITILALIZATION OPERATIONS ========
 int nameCheck(int);
+int size_of_line(char *filename){
+    int size_of_line = 0;
+    char *linem;
+    FILE *vectorFile ;
+    vectorFile= fopen(filename, "r");
+
+    char * line = NULL;
+
+    int i =0;
+    size_t len = 1;
+    getline(&line, &len, vectorFile);
+    size_of_line = (strlen(line)+1)/2;
+    return size_of_line;
+}
 
 
-int * veczeros(int *name, int length){
-    int *vZeros;
+vectors veczeros(char *name, int length, int index, vectors *varr){
 
-    name = (int *)malloc(length* sizeof(int));
-    vZeros = name;
-    int i;
-    for (i  = 0;  i<length ; i++) {
-        vZeros[i] =0;
-    }
+    varr[index].name = name;
+    varr[index].length=length;
+    varr[index].arr = (int*)malloc(sizeof(int)*length);
 
-    return vZeros;
+    //free?
+    return varr[index];
 }
 
 // Zero Vectors Initialized
 
 
-int** matzeros(int **name, int x, int y){
-   int **mZeros = NULL;
-    name= (int **)malloc(sizeof(int) * x);
-    int i;
-    for ( i = 0; i < x; i++) {
-        name[i] = (int *)malloc(y * sizeof(int));
-    }
-    mZeros = name;
-    int j, k;
-    for (j = 0; j < x ; ++j) {
-        for ( k = 0; k < y; ++k) {
-            mZeros[j][k] = 0;
-        }
+matrix matzeros(char *name, int x, int y, int index, matrix *mat){
+   int iter,i,j,k;
 
+   mat[index].name=name;
+   mat[index].x=x;
+   mat[index].y=y;
+   mat[index].arr= (int **)malloc(x * sizeof(int *));
+    for (int i = 0; i < x ; i++) {
+        mat[index].arr[i]= (int *)malloc(y * sizeof(int));
     }
 
-    return mZeros;
+
+    return mat[index];
 }
 
 // ========== ZERO MATRICES INITIALIZED.
 
 
 
-char *vecread(char *filename){
+vectors vecread(char *filename, vectors *vec, int index){
+    char *linem;
     FILE *vectorFile ;
     vectorFile= fopen(filename, "r");
     if(vectorFile == NULL){
@@ -88,32 +98,171 @@ char *vecread(char *filename){
     int i =0;
     size_t len = 1;
     getline(&line, &len, vectorFile);
+    vec[index].name = strtok(filename, ".");
+    vec[index].length = (strlen(line)+1)/2;
+    vec[index].arr = (int*)malloc(sizeof(int)*vec[index].length);
 
 
+    linem = strtok(line," ");
 
-
-
-
-
-
-
-
-    return line;
-
+    while(linem != NULL){
+        vec[index].arr[i]=atoi(linem);
+        linem = strtok(NULL, " " );
+        i++;
+    }
+   return vec[index];
 }
 
-//====== VECTOR READ.
+//   ====== VECTOR READ.
 
 
+matrix matread( char *filename, matrix *mat, int index){
+    int i;
+    FILE *matrixFile ;
+    matrixFile= fopen(filename, "r");
+    if(matrixFile == NULL){
+        printf("Error while opening file.\n");
+    }
 
-int matread(char *filename){
+    int lineNum = lineCount(filename);
+    int lineSize = size_of_line(filename);
+    mat[index].name=strtok(filename, ".");
+    mat[index].x = lineNum;
+    mat[index].y = lineSize;
+    mat[index].arr= (int **)malloc(lineNum * sizeof(int *));
+    for (int i = 0; i < lineNum ; i++) {
+        mat[index].arr[i]= (int *)malloc(lineSize * sizeof(int));
+    }
+    char * line = NULL;
+    size_t len = lineNum;
+    char *command;
+    int j,k;
+    while ((getline(&line, &len, matrixFile)) != -1) {
+        for (j = 0; j < lineNum ; j++) {
+            for (k = 0; k < lineSize; k++) {
+                mat[index].arr[j][k] = atoi(strtok(line, " "));
+            }
+        }
+    }
+    return mat[index];
+}
+//  ================================================
+//  ================================================
+//  ================================================
+//  ================================================
+//  ======== CONCATENATIONS ========================
+//  ================================================
+//  ================================================
+int find_vector_index(char *name, vectors *vArr, int vSize){
+    int i, index;
+    for (i = 0; i <vSize ; i++) {
+        if(strcmp(vArr[i].name, name)==0){
+            return i;
+        }
+    }
     return 0;
 }
-//  ================================================
+matrix vecstack(char *vector1, char *vector2, char *direction, char *name, int index, matrix *mat,vectors *v, int vSize){
+    if(strcmp(direction, "row") == 0 && v[find_vector_index(vector1, v, vSize)].length == v[find_vector_index(vector2, v, vSize)].length){
+        mat[index].name=name;
+        mat[index].x = 2;
+        mat[index].y = v[find_vector_index(vector1, v, vSize)].length;
+        mat[index].arr = (int **)malloc(2 * sizeof(int *));
+        for (int i = 0; i < 2 ; i++) {
+            mat[index].arr[i]= (int *)malloc(mat[index].y * sizeof(int));
+        }
+        int y;
+        for ( y = 0; y < mat[index].y ; y++) {
+                mat[index].arr[0][y] = v[find_vector_index(vector1, v, vSize)].arr[y];
+        }
+        for ( y = 0; y < mat[index].y ; y++) {
+                mat[index].arr[1][y] = v[find_vector_index(vector2, v, vSize)].arr[y];
+        }
 
 
-//  ======== CONCATENATIONS ========
-//  ================================================
+
+    }else if(strcmp(direction, "column") == 0 && v[find_vector_index(vector1, v, vSize)].length == v[find_vector_index(vector2, v, vSize)].length){
+        mat[index].name=name;
+        mat[index].x=  v[find_vector_index(vector1, v, vSize)].length;
+        mat[index].y=2;
+        mat[index].arr = (int **)malloc(mat[index].x * sizeof(int *));
+        for (int i = 0; i < 2 ; i++) {
+            mat[index].arr[i]= (int *)malloc(2 * sizeof(int));
+        }
+        int i,j;
+        for ( i = 0; i < mat[index].x; ++i) {
+            mat[index].arr[i][0] = v[find_vector_index(vector1, v, vSize)].arr[i];
+        }
+        for ( i = 0; i < mat[index].x; ++i) {
+            mat[index].arr[i][1] = v[find_vector_index(vector2, v, vSize)].arr[i];
+        }
+
+    }else{
+        printf("error12122");
+    }
+    return mat[index];
+
+}
+// vecstack function
+// ================================================
+int find_matrix_index(char *name, matrix *mArr,int mSize){
+    int i, index;
+    for (i = 0; i <mSize ; i++) {
+        if(strcmp(mArr[i].name, name)==0){
+            return i;
+        }
+    }
+    return 0;
+
+}
+matrix matstack(char *mat1, char *mat2, char *where, matrix *m, int mSize ){
+    int i1,i2;
+    i1 = find_matrix_index(mat1, m, mSize);
+    i2 = find_matrix_index(mat2, m, mSize);
+    int old_size_m1x = m[find_matrix_index(mat1, m, mSize)].x;
+    int old_size_m1y = m[find_matrix_index(mat1, m, mSize)].y;
+    int old_Size_m2x = m[find_matrix_index(mat2, m, mSize)].x;
+    int old_Size_m2y = m[find_matrix_index(mat2, m, mSize)].y;
+    if(strcmp(where,"d")==0 && m[i1].y == m[i2].y){
+        int new_size_x= old_size_m1x + old_Size_m2x;
+        int new_size_y= old_size_m1y;
+        int **p = m[i1].arr;
+        p = (int **)realloc(p,new_size_x);
+        int i;
+        for (i = 0; i < new_size_y ; i++) {
+            *(p+i) = (int *)realloc(*(p+i) , sizeof(int) * new_size_x);
+        }
+        int k,l;
+        for ( k = 0; k < old_Size_m2x ; k++) {
+            for (l = 0; l < new_size_y ; l++) {
+                m[i1].arr[new_size_x-old_size_m1x+k][l] = m[i2].arr[k][l];
+            }
+        }
+        return m[i1];
+//                for ( i = 0; i < m[i2].x ; i++) {
+//                    for (int j = 0; j < m[i2].y ; j++) {
+//                        m[i1].arr[m[i1].x-m[i2].x+i][j] = m[i2].arr[i][j];
+//                    }
+//
+//        }
+//        return m[i1];
+    }
+    else if(strcmp(where,"r")==0 && m[i1].x == m[i2].x){
+        int new_size_x = old_size_m1x;
+        int new_size_y = old_size_m1y+old_Size_m2y;
+        int **p = m[i1].arr;
+        p=(int **)realloc(p, sizeof(int) * new_size_x);
+        int i;
+        for (i = 0; i < new_size_y; i++) {
+            *(p+i) = (int *)realloc(*(p+i) , sizeof(int) * new_size_x);
+        }
+    }
+    else{printf("error");}
+
+
+
+    return m[0];
+}
 
 
 //  ======== PADDINGS ========
@@ -154,7 +303,7 @@ int main(){
     int matCount = 0;
     // variable for counters.
 
-    char *commands = "inputs/commands2.txt";
+    char *commands = "commands4.txt";
 
     // File Reading
 
@@ -197,7 +346,7 @@ int main(){
     // vector and matrix arrays defined.
 
     // functions directors below.
-
+    int mIndex = 0; int vIndex =0;
     char *ch;
     int k;
     for (k= 0; k < lineNum; k++) {
@@ -208,80 +357,84 @@ int main(){
         else if(strcmp(ch,FUNCTIONS[0]) == 0){ //veczeros
 
 
-            int *vZeros=NULL;
             ch=strtok(NULL, " ");
             char *vname= ch;
             ch=strtok(NULL, " ");
             int vlen=atoi(ch);
-            printf("created vector %s %d\n", vname, vlen);
-            int l;
-            vZeros = veczeros(vZeros, vlen);
-            for (l = 0; l < vlen; ++l) {
-                printf("%d",vZeros[l]);
-            }
-            printf("\n");
-            free(vZeros);
+            vectors v=veczeros(vname, vlen, vIndex,vectorArray);
+            printf("created vector %s %d\n", v.name, v.length);
+            int iter;
+            for (iter = 0; iter <v.length ; iter++) {
+                v.arr[iter]=0;
+                printf("%d ", v.arr[iter]);
+            }printf("\n");
 
+//
+        vIndex++;
 
         }else if(strcmp(ch,FUNCTIONS[1]) == 0){ //matzeros
-            int **mZeros=NULL;
             ch=strtok(NULL, " ");
             char *name = ch;
             ch=strtok(NULL, " ");
             int row = atoi(ch);
             ch=strtok(NULL, " ");
             int col = atoi(ch);
-
-            printf("created matrix %s %d %d\n", name,row,col);
-            mZeros= matzeros(mZeros, row, col);
-            int mzRow, mzCol;
-            for ( mzRow = 0; mzRow < row; mzRow++) {
-                for(mzCol =0; mzCol <col; mzCol++){
-                    printf("%d",mZeros[mzRow][mzCol]);
+            matrix m=matzeros(name, row, col, mIndex, matrixArray);
+            printf("created matrix %s %d %d\n", m.name,m.x,m.y);
+            for(j=0; j<m.x; j++){
+                for(k=0; k<m.y; k++){
+                    m.arr[j][k] = 0;
+                    printf("%d ", m.arr[j][k]);
                 }
                 printf("\n");
             }
-            free(mZeros);
 
+        mIndex++;
         }else if(strcmp(ch,FUNCTIONS[2]) == 0){ // vecread
-            char *linem;
-            char *line = NULL;
+
             ch=strtok(NULL, " ");
             char *readVectorName =strtok(ch,"\n");
-            line = vecread(readVectorName);
-            int *vector = NULL;
-            vector = (int*)malloc(sizeof(int)*((strlen(line)+1)/2));
-            int count = (strlen(line)+1)/2;
-            linem = strtok(line," ");
-            int i =0;
-            while(linem != NULL){
-                vector[i]=atoi(linem);
-                linem = strtok(NULL, " " );
-                i++;
-            }
-
-            printf("read vector %s %d \n", readVectorName, count);
+            vectors v = vecread(readVectorName, vectorArray, vIndex);
+            printf("read vector %s %d \n", v.name, v.length);
             int l;
-            for ( l = 0; l < count-1; l++) {
-                printf("%d ", vector[l]);
+            for ( l = 0; l < v.length-1; l++) {
+                printf("%d ", v.arr[l]);
             }printf("\n");
-
-            free(vector);
-
+        vIndex++;
 
 
-        }else if(strcmp(ch,FUNCTIONS[3]) == 0){
-            int *matrix = NULL;
+        }else if(strcmp(ch,FUNCTIONS[3]) == 0){ //matread
             ch=strtok(NULL, " ");
             char *readMatrixName=strtok(ch, "\n");
+            matrix m=  matread(readMatrixName, matrixArray, mIndex);
+            printf("read matrix %s %d %d\n",m.name,m.x,m.y);
+            int t,y;
+            for ( t = 0; t < m.x ; t++) {
+                for ( y = 0; y < m.y ; y++) {
+                    printf("%d ",m.arr[t][y]);
+                }printf("\n");
+            }
+        mIndex++;
 
-            printf("%s\n", ch);
-
-        }else if(strcmp(ch,FUNCTIONS[4]) == 0){
+        }else if(strcmp(ch,FUNCTIONS[4]) == 0){ //vecstack
             ch=strtok(NULL, " ");
-            printf("%s\n", ch);
-
-        }else if(strcmp(ch,FUNCTIONS[5]) == 0){
+            char *v1 = ch;
+            ch=strtok(NULL, " ");
+            char *v2 = ch;
+            ch=strtok(NULL, " ");
+            char *direction= ch;
+            ch=strtok(NULL, " ");
+            char *name = ch;
+            matrix m = vecstack(v1,v2,direction,name,mIndex,matrixArray,vectorArray,matCount);
+            printf("vectors concatenated %s %d %d\n",m.name,m.x,m.y );
+            int a,b;
+            for (a = 0; a < m.x ; a++) {
+                for (int b = 0; b <m.y ; b++) {
+                    printf("%d ", m.arr[a][b]);
+                }printf("\n");
+            }
+        mIndex++;
+        }else if(strcmp(ch,FUNCTIONS[5]) == 0){ //matstack
             ch=strtok(NULL, " ");
             printf("%s\n", ch);
 
